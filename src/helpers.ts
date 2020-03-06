54849d142600d3ea9f0c4081e53da00e741cba58
Marc Cataford <marc.cataford@tophatmonocle.com>
2020-03-06 17:46:20 -0500

    wip: add partial pattern matching

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,3 +4,18 @@ export function isValidSourceFile(path: string): boolean {
     const pattern = SOURCE_FILE_PATTERN
     return pattern.test(path)
 }
+
+export function isPartialMatch(left: string, right: string): boolean {
+    const prefixPattern = new RegExp(`^${left}`)
+    return prefixPattern.test(right)
+}
+
+/*
+export function isBuiltIn(packageName: string): boolean {
+    try {
+        return !require.resolve(packageName).includes('node_modules')
+    } catch (e) {
+        console.log(e)
+        return false
+    }
+}*/
