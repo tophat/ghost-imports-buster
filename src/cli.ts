f6acb5045f20c0dac09a8d35503850b58afa95e9
Marc Cataford <c.marcandre@gmail.com>
2021-02-26 00:14:18 -0500

    wip: args

--- src/cli.ts
+++ src/cli.ts
@@ -1,24 +1,11 @@
 #!/usr/bin/env node
 
-import program from 'commander'
+import yargs from 'yargs'
 
 import validateDependencies from '.'
 
-const parseCliArgs = (args: string[]): string[] => {
-    let pathsToProcess: string[] = []
+const argv = yargs(process.argv.slice(2)).usage(
+    'ghostimports [projectRootPath]',
+).argv
 
-    program
-        .command('validate <target> [otherTargets...]')
-        .option('-e', '--throwOnFail', 'Throws an error on failure.')
-        .action((target: string, otherTargets: string[]) => {
-            pathsToProcess = otherTargets ? [target, ...otherTargets] : [target]
-        })
-
-    program.parse(args)
-
-    return pathsToProcess
-}
-
-//const cliArgs = process.argv
-//const runParams = parseCliArgs()
-validateDependencies({ cwd: process.argv[2] })
+validateDependencies({ cwd: argv._[0] as string }).catch(() => process.exit(1))
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import chalk from 'chalk'
-
 import {
     getContext,
     getDependenciesByWorkspaceMap,
