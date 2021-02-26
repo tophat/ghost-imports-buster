e91be1a02edf6e9c92fb8b02e33a4aac59f251e0
Marc Cataford <c.marcandre@gmail.com>
2021-02-26 10:46:45 -0500

    wip: node<14 compat

--- babel.config.js
+++ babel.config.js
@@ -8,5 +8,5 @@ module.exports = {
         ],
         '@babel/preset-typescript',
     ],
-    plugins: ['@babel/plugin-proposal-optional-chaining'],
+    plugins: ["@babel/plugin-proposal-nullish-coalescing-operator", '@babel/plugin-proposal-optional-chaining'],
 }
