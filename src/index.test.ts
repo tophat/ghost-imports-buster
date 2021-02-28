61d4996fb06676570d8ac9a7b2fe44e06612ea77
Marc Cataford <c.marcandre@gmail.com>
2021-02-28 13:36:23 -0500

    test: high-level cov

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,30 +1,173 @@
+import { promises as fs } from 'fs'
+import { tmpdir } from 'os'
 import { resolve } from 'path'
 
 import validateDependencies from '.'
 
-describe('', () => {
-    const cwd = process.cwd()
+const TEST_PACKAGE_NAME = 'testpackage'
 
-    it('with no missing dependencies', async () => {
-        const projectPath = resolve(cwd, 'example-repos/no-missing-deps')
-        const report = await validateDependencies({ cwd: projectPath })
+async function prepareTempDirectory(): Promise<string> {
+    const tempRoot = await fs.mkdtemp(`${tmpdir()}/`)
 
-        expect(report.undeclaredDependencies.size).toEqual(1)
-        expect(
-            report.undeclaredDependencies.get('no-missing-deps').size,
-        ).toEqual(0)
-        expect(report.unusedDependencies.size).toEqual(1)
-        expect(report.unusedDependencies.get('no-missing-deps').size).toEqual(0)
+    // A lockfile is needed, but its content don't matter.
+    await createFile(tempRoot, 'yarn.lock', '')
+
+    const packageJsonTemplate = {
+        name: TEST_PACKAGE_NAME,
+        dependencies: {},
+        devDependencies: {},
+        peerDependencies: {},
+    }
+    // Basic package.json without dependencies
+    await createFile(
+        tempRoot,
+        'package.json',
+        JSON.stringify(packageJsonTemplate),
+    )
+
+    return tempRoot
+}
+
+async function declareDependencies(
+    cwd: string,
+    dependencies: Map<string, string>,
+): Promise<Map<string, string>> {
+    const currentPackageJson = JSON.parse(
+        await fs.readFile(resolve(cwd, 'package.json'), { encoding: 'utf8' }),
+    )
+
+    for (const dependency of dependencies.entries()) {
+        const [packageName, dependencyClass] = dependency
+
+        if (dependencyClass === 'prod')
+            currentPackageJson.dependencies[packageName] = '*'
+        else if (dependencyClass === 'dev')
+            currentPackageJson.devDependencies[packageName] = '*'
+        else if (dependencyClass === 'peer')
+            currentPackageJson.peerDependencies[packageName] = '*'
+    }
+
+    await createFile(cwd, 'package.json', JSON.stringify(currentPackageJson))
+
+    return currentPackageJson
+}
+
+async function createFile(
+    cwd: string,
+    path: string,
+    data: string,
+): Promise<string> {
+    const filePath = resolve(cwd, path)
+    await fs.writeFile(filePath, data, { encoding: 'utf8' })
+
+    return filePath
+}
+
+describe('GhostImports', () => {
+    let workspacePath
+
+    beforeEach(async () => {
+        workspacePath = await prepareTempDirectory()
     })
 
-    it('with missing dependencies', async () => {
-        const projectPath = resolve(cwd, 'example-repos/missing-deps')
-        const report = await validateDependencies({ cwd: projectPath })
-        expect(report.undeclaredDependencies.size).toEqual(1)
-        expect(report.undeclaredDependencies.get('missing-deps').size).toEqual(
-            1,
-        )
-        expect(report.unusedDependencies.size).toEqual(1)
-        expect(report.unusedDependencies.get('missing-deps').size).toEqual(0)
+    afterEach(async () => {
+        await fs.rmdir(workspacePath, { recursive: true })
+    })
+    describe('High-level use cases (single project)', () => {
+        it('detects require imports correctly', async () => {
+            const cwd = workspacePath
+            const dependencies = new Map([
+                ['pkg-1', 'prod'],
+                ['pkg-3', 'prod'],
+            ])
+            await declareDependencies(cwd, dependencies)
+            await createFile(
+                cwd,
+                'index.js',
+                `
+                     const { foo } = require("pkg-1")
+                     const { bar } = require("pkg-2")
+
+                     foo()`,
+            )
+            const report = await validateDependencies({ cwd })
+
+            expect(
+                report.undeclaredDependencies.get(TEST_PACKAGE_NAME),
+            ).toEqual(new Set(['pkg-2']))
+            expect(report.unusedDependencies.get(TEST_PACKAGE_NAME)).toEqual(
+                new Set(['pkg-3']),
+            )
+        })
+
+        it('produces report when no undeclared dependencies', async () => {
+            const cwd = workspacePath
+            const dependencies = new Map([['pkg-1', 'prod']])
+            await declareDependencies(cwd, dependencies)
+            await createFile(
+                cwd,
+                'index.js',
+                `
+                     import { foo } from "pkg-1"
+
+                     foo()`,
+            )
+            const report = await validateDependencies({ cwd })
+
+            expect(
+                report.undeclaredDependencies.get(TEST_PACKAGE_NAME).size,
+            ).toEqual(0)
+            expect(
+                report.unusedDependencies.get(TEST_PACKAGE_NAME).size,
+            ).toEqual(0)
+        })
+
+        it('produces report when undeclared dependencies are present', async () => {
+            const cwd = workspacePath
+            const dependencies = new Map([['pkg-1', 'prod']])
+            await declareDependencies(cwd, dependencies)
+            await createFile(
+                cwd,
+                'index.js',
+                `
+                     import { foo } from "pkg-1"
+                     import { bar } from "pkg-2"
+
+                     foo()`,
+            )
+            const report = await validateDependencies({ cwd })
+
+            expect(
+                report.undeclaredDependencies.get(TEST_PACKAGE_NAME),
+            ).toEqual(new Set(['pkg-2']))
+            expect(
+                report.unusedDependencies.get(TEST_PACKAGE_NAME).size,
+            ).toEqual(0)
+        })
+
+        it('produces report when unused dependencies are present', async () => {
+            const cwd = workspacePath
+            const dependencies = new Map([
+                ['pkg-1', 'prod'],
+                ['pkg-2', 'prod'],
+            ])
+            await declareDependencies(cwd, dependencies)
+            await createFile(
+                cwd,
+                'index.js',
+                `
+                     import { foo } from "pkg-1"
+
+                     foo()`,
+            )
+            const report = await validateDependencies({ cwd })
+
+            expect(report.unusedDependencies.get(TEST_PACKAGE_NAME)).toEqual(
+                new Set(['pkg-2']),
+            )
+            expect(
+                report.undeclaredDependencies.get(TEST_PACKAGE_NAME).size,
+            ).toEqual(0)
+        })
     })
 })
