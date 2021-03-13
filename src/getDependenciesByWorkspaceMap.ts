3d28de2721225781528c34c62d21b2df2165ac46
noahnu <noahnu@gmail.com>
2021-03-12 20:39:46 -0500

    wip: collect transitive peer dependencies

--- src/getDependenciesByWorkspaceMap.ts
+++ src/getDependenciesByWorkspaceMap.ts
@@ -1,6 +1,36 @@
-import { Descriptor, IdentHash, Workspace } from '@yarnpkg/core'
+import { Descriptor, IdentHash, LocatorHash, Workspace } from '@yarnpkg/core'
 
 import { Context, DependenciesMap } from './types'
+
+async function getTransitivePeerDependencies(
+    workspace: Workspace,
+): Promise<Map<IdentHash, Descriptor>> {
+    const transitivePeerDeps = new Map<IdentHash, Descriptor>()
+
+    const manifest = workspace.manifest
+    const locatorHashes = [
+        ...manifest.getForScope('dependencies').values(),
+        ...manifest.getForScope('devDependencies').values(),
+        ...manifest.getForScope('peerDependencies').values(),
+    ]
+        .map((descriptor) =>
+            workspace.project.storedResolutions.get(descriptor.descriptorHash),
+        )
+        .filter((hash: LocatorHash | undefined): hash is LocatorHash =>
+            Boolean(hash),
+        )
+
+    for (const locatorHash of locatorHashes) {
+        const pkg = workspace.project.storedPackages.get(locatorHash)
+        if (!pkg) continue
+
+        for (const [identHash, descriptor] of pkg.peerDependencies.entries()) {
+            transitivePeerDeps.set(identHash, descriptor)
+        }
+    }
+
+    return transitivePeerDeps
+}
 
 export default async function getDependenciesByWorkspaceMap(
     context: Context,
@@ -13,7 +43,9 @@ export default async function getDependenciesByWorkspaceMap(
             dependencies: manifest.getForScope('dependencies'),
             devDependencies: manifest.getForScope('devDependencies'),
             peerDependencies: manifest.getForScope('peerDependencies'),
-            transitivePeerDependencies: new Map<IdentHash, Descriptor>(),
+            transitivePeerDependencies: await getTransitivePeerDependencies(
+                workspace,
+            ),
         })
     }
 
