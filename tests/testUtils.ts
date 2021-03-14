625c12d4058e3b90586f94489e3f001f6191d18c
noahnu <noahnu@gmail.com>
2021-03-14 12:43:12 -0400

    wip: install project

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -1,14 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { promises as fs } from 'fs'
 import { tmpdir } from 'os'
-import { dirname, resolve } from 'path'
+import path from 'path'
+
+import { npath } from '@yarnpkg/fslib'
+import { Cache, Configuration, Project, ThrowReport } from '@yarnpkg/core'
+import { getPluginConfiguration } from '@yarnpkg/cli'
 
 // FIXME: better typing?
 interface ProjectOverrides {
     [key: string]: any
 }
 
-type Callback = (...args: unknown[]) => unknown
+type Callback = (...args: unknown[]) => Promise<unknown>
 
 export async function withDirectoryContext(
     projectOverrides: ProjectOverrides,
@@ -33,6 +37,12 @@ export async function withDirectoryContext(
                 peerDependencies: projectOverride?.peerDependencies ?? {},
             }
 
+            if (isMonorepo && isMonorepoRoot) {
+                packageManifest.workspaces = packageManifest.workspaces || [
+                    'packages/*',
+                ]
+            }
+
             const projectPath =
                 isMonorepo && !isMonorepoRoot
                     ? `${tempRoot}/root/packages/${projectName}/`
@@ -40,10 +50,48 @@ export async function withDirectoryContext(
             await createFile(
                 projectPath,
                 'package.json',
-                JSON.stringify(packageManifest),
+                JSON.stringify(packageManifest, null, 2),
             )
-            await createFile(projectPath, 'yarn.lock', '')
+
+            if ((isMonorepo && isMonorepoRoot) || !isMonorepo) {
+                const releasesDir = path.join(
+                    __dirname,
+                    '..',
+                    '.yarn',
+                    'releases',
+                )
+                const yarnBinary = path.resolve(
+                    path.join(releasesDir, 'yarn-sources.cjs'),
+                )
+                await fs.symlink(
+                    yarnBinary,
+                    path.join(projectPath, 'run-yarn.cjs'),
+                )
+                await fs.writeFile(
+                    path.join(projectPath, '.yarnrc.yml'),
+                    `yarnPath: ./run-yarn.cjs\nenableGlobalCache: false`,
+                    'utf-8',
+                )
+                await fs.writeFile(
+                    path.join(projectPath, 'yarn.lock'),
+                    ``,
+                    'utf-8',
+                )
+                const configuration = await Configuration.find(
+                    npath.toPortablePath(projectPath),
+                    getPluginConfiguration(),
+                )
+                const { project } = await Project.find(
+                    configuration,
+                    npath.toPortablePath(projectPath),
+                )
+                await project.install({
+                    cache: await Cache.find(configuration),
+                    report: new ThrowReport(),
+                })
+            }
         }
+
         await callback(rootPath)
     } catch (e) {
         error = e
@@ -59,17 +107,17 @@ export async function cleanUp(paths: string[]): Promise<void> {
     )
 }
 
-export async function readFile(path: string): Promise<string> {
-    return fs.readFile(path, { encoding: 'utf8' })
+export async function readFile(filename: string): Promise<string> {
+    return fs.readFile(filename, { encoding: 'utf8' })
 }
 
 export async function createFile(
     cwd: string,
-    path: string,
+    filename: string,
     data: string,
 ): Promise<string> {
-    const filePath = resolve(cwd, path)
-    const parentDirectory = dirname(filePath)
+    const filePath = path.resolve(cwd, filename)
+    const parentDirectory = path.dirname(filePath)
     await fs.mkdir(parentDirectory, { recursive: true })
     await fs.writeFile(filePath, data, { encoding: 'utf8' })
 
