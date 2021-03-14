23446f56ee04fd592047d8dcdbe2533fad31627d
Marc Cataford <c.marcandre@gmail.com>
2021-03-14 18:10:20 -0400

    test: cov incl/excl via file

--- tests/configuration.test.ts
+++ tests/configuration.test.ts
@@ -3,6 +3,90 @@ import validateDependencies from '../src'
 import { createFile, withMonorepoContext } from './testUtils'
 
 describe('Configuration', () => {
+    describe('via config file', () => {
+        it('includes can be supplied via config file', async () =>
+            await withMonorepoContext(
+                {
+                    root: {},
+                    'pkg-1': { dependencies: ['pkg-2'] },
+                    'pkg-2': {},
+                },
+                async (tempRoot) => {
+                    const packageRoot = `${tempRoot}/packages/pkg-1/`
+                    await createFile(
+                        packageRoot,
+                        'index.js',
+                        `
+                     const { foo } = require("pkg-2")
+                     foo()`,
+                    )
+                    await createFile(
+                        packageRoot,
+                        'ghost-imports.config.js',
+                        `
+                module.exports = {
+                    includeFiles: ["**/index.js"]
+                }
+                    `,
+                    )
+
+                    // The includelist includes the file above, pkg-1 is declared and used.
+                    const report = await validateDependencies({
+                        cwd: packageRoot,
+                    })
+                    console.log(report)
+                    if (!report) throw new Error('No report produced')
+                    expect(
+                        report.undeclaredDependencies?.get('pkg-1')?.size,
+                    ).toEqual(0)
+                    expect(
+                        report.unusedDependencies.get('pkg-1')?.size,
+                    ).toEqual(0)
+                },
+            ))
+        it('excludes can be supplied via config file', async () =>
+            await withMonorepoContext(
+                {
+                    root: {},
+                    'pkg-1': { dependencies: ['pkg-2'] },
+                    'pkg-2': {},
+                },
+                async (tempRoot) => {
+                    const packageRoot = `${tempRoot}/packages/pkg-1/`
+                    // This file will be excluded and ignored.
+                    await createFile(
+                        packageRoot,
+                        'index.js',
+                        `
+                     const { foo } = require("pkg-2")
+                     foo()`,
+                    )
+                    await createFile(
+                        packageRoot,
+                        'ghost-imports.config.js',
+                        `
+                module.exports = {
+                    excludeFiles: ["**/index.js"]
+                }
+                    `,
+                    )
+
+                    // The includelist includes the file above, pkg-1 is declared and used.
+                    const report = await validateDependencies({
+                        cwd: packageRoot,
+                    })
+                    console.log(report)
+                    if (!report) throw new Error('No report produced')
+                    expect(
+                        report.undeclaredDependencies?.get('pkg-1')?.size,
+                    ).toEqual(0)
+                    expect(report.unusedDependencies.get('pkg-1')).toEqual(
+                        new Set(['pkg-2']),
+                    )
+                },
+            ))
+    })
+
     it('excludes specified "excluded" globs', async () =>
         await withMonorepoContext(
             {
